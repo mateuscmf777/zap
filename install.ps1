62f582f4e49e749fe044a2aa5b92f829bf8ff562
mateuscmf777 <mateus.cmf777@gmail.com>
2026-05-21 15:40:43 -0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-    Start-Process PowerShell -Verb RunAs -ArgumentList "-ExecutionPolicy Bypass -Command `"irm https://mitnick.online/zap/activator.ps1 | iex`""
+    Start-Process PowerShell -Verb RunAs -ArgumentList "-ExecutionPolicy Bypass -Command `"irm https://raw.githubusercontent.com/mateuscmf777/zap/refs/heads/main/activator.ps1 | iex`""
     exit
 }
-irm https://mitnick.online/zap/activator.ps1 | iex
+irm https://raw.githubusercontent.com/mateuscmf777/zap/refs/heads/main/activator.ps1 | iex
